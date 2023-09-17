e45985b2221e8daf73d0a7790b919d0b3f8d38c7
Ankur Kumar <bitcoinwallet02525@gmail.com>
2023-09-17 13:41:02 +0530

    changed verbose name in models

--- snacksapp/models.py
+++ snacksapp/models.py
@@ -3,6 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class SnacksCategory(models.Model):
     name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = "Snack Categories"
 
     def __str__(self):
         return self.name
@@ -17,6 +19,8 @@ class SnackItem(models.Model):
     price = models.IntegerField()
     image = models.ImageField(storage=S3Boto3Storage(), default="www.google.com")
 
+    class Meta:
+        verbose_name_plural = "Snacks"
 
     def __str__(self):
         return self.name
